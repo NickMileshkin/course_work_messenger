fb5f519c54c06b5808c9d409e247844219a45d91
Fescoret <viktor.fesenk0@mail.ru>
2021-05-07 11:03:47 +0700

    add database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+class AccountDatabase:
+
+    def add_new_account(self, id_, login, password):
+        pass
+
+    def change_password(self, login, password, new_password):
+        pass
+
+    def change_login(self, login, password, new_login):
+        pass
--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello_world', methods = ['GET'])
+
+@app.route('/hello_world', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
 
-if (__name__ == '__main__'):
+
+if __name__ == '__main__':
     app.run()
