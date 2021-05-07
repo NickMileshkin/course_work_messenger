d97acfa9edd0cac79113ce200759e39333d32351
Fescoret <viktor.fesenk0@mail.ru>
2021-05-07 12:40:39 +0700

    realise __init__() and update get_account()

--- database.py
+++ database.py
@@ -1,7 +1,25 @@
+import sqlite3
+
+
 class AccountDatabase:
 
+    def __init__(self, database: str = "accounts.sqlite"):
+        self._db = database
+        with sqlite3.connect(self._db) as connection:
+            cursor = connection.cursor()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS accounts (
+                              id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                              login TEXT NOT NULL,
+                              password TEXT NOT NULL);""")
+            connection.commit()
+        connection.close()
+
     def get_account(self):
-        pass
+        with sqlite3.connect(self._db) as connection:
+            cursor = connection.cursor()
+            result = cursor.execute("""SELECT id, login, password
+                                       FROM accounts""").fetchall()
+        return result
 
     def add_new_account(self, id_, login, password) -> bool:
         pass
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from database import AccountDatabase
 
 app = Flask(__name__)
+db = AccountDatabase()
 
 
 @app.route('/hello_world')
@@ -10,22 +12,23 @@ def hello_world():
 
 @app.route('/accounts', methods=['GET'])
 def get_account():
-    return db.get_account()
+    print(db.get_account())
+    return "db.get_account()"
 
 
 @app.route('/accounts', methods=['POST'])
 def add_new_account():
-    return db.add_new_account(id_, login, password)
+    return "db.add_new_account(id_, login, password)"
 
 
-@app.route('/accounts/<int: account_id/password', methods=['POST'])
+@app.route('/accounts/<int:account_id>/password', methods=['POST'])
 def change_password(account_id):
-    return db.change_password(account_id, new_password)
+    return "db.change_password(account_id, new_password)"
 
 
-@app.route('/accounts/<int: account_id/login', methods=['POST'])
+@app.route('/accounts/<int:account_id>/login', methods=['POST'])
 def change_login(account_id):
-    return db.change_login(account_id, new_login)
+    return "db.change_login(account_id, new_login)"
 
 
 if __name__ == '__main__':
