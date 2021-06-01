5e54f14b219f190d8beda1bae514ee16ea7f0a0c
Fescoret <viktor.fesenk0@mail.ru>
2021-06-01 14:46:42 +0700

    add add_dialog()

--- clientDB.py
+++ clientDB.py
@@ -8,7 +8,7 @@ class ClientDatabase:
         with sqlite3.connect(self._db) as connection:
             cursor = connection.cursor()
             cursor.execute("""CREATE TABLE IF NOT EXISTS dialogs (
-                              dialog_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                              dialog_id INTEGER NOT NULL PRIMARY KEY,
                               account_id INTEGER NOT NULL);""")
             cursor.execute("""CREATE TABLE IF NOT EXISTS messages (
                               message_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -26,3 +26,10 @@ class ClientDatabase:
             result = cursor.execute("""SELECT dialog_id, account_id
                                        FROM dialogs""").fetchall()
         return result
+
+    def add_dialog(self, dialog_id, account_id):
+        with sqlite3.connect(self._db) as connection:
+            cursor = connection.cursor()
+            result = cursor.execute("""INSERT INTO dialogs (dialog_id, account_id)
+                                       VALUES (?, ?)""", (dialog_id, account_id))
+            connection.commit()
